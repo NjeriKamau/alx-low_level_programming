c88a1bb2244fa1770ffe434543db798b33a4ea84
NjeriKamau <mukurutracey@gmail.com>
2022-03-10 12:00:07 -0800

    C File Put INstead of Print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Entry Point
+* Function that prints a string
+* return always an int value.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
